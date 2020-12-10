172c04fb45765b09d0282ffd2713b22fb694132c
ZigZagZap10 <58115931+ZigZagZap10@users.noreply.github.com>
2020-12-10 10:56:46 -0600

    code

---
+++ Scripts/LevelLock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class LevelLock : MonoBehaviour
+{
+    public Button[] Buttons;
+    public int UnlockedCount;
+
+    public void Update()
+    {
+        for (int i = 0; UnlockedCount > i; i++)
+        {
+            Buttons[i].interactable = true;
+        }
+    }
+}
---
+++ Scripts/Sound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound
+{
+    // this script creates a Sound class that stores the following values.
+
+    public string title;
+    public AudioClip clip;
+
+    [Range(0f, 1f)]
+    public float volume;
+    [Range(0.1f, 3f)]
+    public float pitch;
+
+    public int priority;
+
+    [HideInInspector]
+    public AudioSource source;
+}
